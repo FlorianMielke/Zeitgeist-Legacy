364cb01a5f748af6d9471ec959e77b685740ab7a
Florian Mielke <f.mielke@systemweit.de>
2018-03-28 20:14:30 +0200

    Add current

--- Zeitgeist/Period.swift
+++ Zeitgeist/Period.swift
@@ -4,6 +4,7 @@ public struct Period: Equatable, Hashable, Comparable {
     public let starts: Time
     public let ends: Time
     public let allDay: Bool
+    public static let current = Period(starts: Time(), ends: Time(), allDay: false)
     
     public static func <(lhs: Period, rhs: Period) -> Bool {
         return lhs.starts < rhs.starts && lhs.ends < rhs.ends
