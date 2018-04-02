64e42901b33bdb9763499a9c1450186946e077f8
Florian Mielke <f.mielke@systemweit.de>
2018-04-02 22:07:50 +0200

    Add rounding

--- Zeitgeist/Int.swift
+++ Zeitgeist/Int.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// MARK: Enumerating
 public extension Int {
     func times(_ f: () -> ()) {
         if self > 0 {
@@ -15,5 +16,21 @@ public extension Int {
                 f()
             }
         }
+    }
+}
+
+// MARK: Rounding
+public extension Int {
+    func roundedUp(to roundTo: Int) -> Int {
+        let remainder = self % roundTo
+        if (remainder == 0) {
+            return self
+        }
+        return self + roundTo - remainder
+    }
+
+    func roundedDown(to roundTo: Int) -> Int {
+        let remainder = self % roundTo
+        return self - remainder
     }
 }
--- ZeitgeistTests/IntTest.swift
+++ ZeitgeistTests/IntTest.swift
@@ -2,6 +2,8 @@ import XCTest
 @testable import Zeitgeist
 
 class IntTest: XCTestCase {
+    // MARK: Enumerating
+    
     func testItCallsAClosure0Times() {
         var result = "a"
         
@@ -30,5 +32,25 @@ class IntTest: XCTestCase {
         }
         
         XCTAssertEqual("abbbbb", result)
+    }
+    
+    // MARK: Rounded
+    
+    func testItRoundsUp() {
+        XCTAssertEqual(0, 0.roundedUp(to: 5))
+        XCTAssertEqual(5, 1.roundedUp(to: 5))
+        XCTAssertEqual(5, 5.roundedUp(to: 5))
+        XCTAssertEqual(10, 6.roundedUp(to: 5))
+        XCTAssertEqual(10, 8.roundedUp(to: 10))
+        XCTAssertEqual(20, 14.roundedUp(to: 10))
+    }
+    
+    func testItRoundsDown() {
+        XCTAssertEqual(0, 0.roundedDown(to: 5))
+        XCTAssertEqual(0, 1.roundedDown(to: 5))
+        XCTAssertEqual(5, 5.roundedDown(to: 5))
+        XCTAssertEqual(5, 8.roundedDown(to: 5))
+        XCTAssertEqual(25, 28.roundedDown(to: 5))
+        XCTAssertEqual(20, 20.roundedDown(to: 10))
     }
 }
