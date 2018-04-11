710d3b524f86d6814d790a56268c180ecb0ea5bf
Florian Mielke <f.mielke@systemweit.de>
2018-04-11 18:32:57 +0200

    Implement travelTo:timeZone:

--- Zeitgeist/Time.swift
+++ Zeitgeist/Time.swift
@@ -11,22 +11,32 @@ public enum Weekday: Int {
 }
 
 public final class Travel {
-    public static var now = Date()
-    
-    public static func travel(to: Date) {
-        now = to
+    static var traveledTo: Date?
+    static var timeZone = TimeZone.current
+    public static var now: Date {
+        return traveledTo ?? Date()
+    }
+    public static var isTraveled: Bool {
+        return traveledTo != nil
+    }
+
+    public static func travel(to: Date, in timeZone: TimeZone = TimeZone.current) {
+        traveledTo = to
+        self.timeZone = timeZone
     }
 
     public static func travelBack() {
-        now = Date()
+        traveledTo = nil
     }
 }
 
 public struct Time: Equatable, Hashable, Comparable {
-    let date: Date
     let calendar: Calendar
     let timeZone: TimeZone
-    public static let current = Time()
+    public let date: Date
+    public static var current: Time {
+        return Time()
+    }
     
     public init(date: Date = Travel.now, timeZone: TimeZone = TimeZone.current) {
         self.date = date
--- ZeitgeistTests/TravelTest.swift
+++ ZeitgeistTests/TravelTest.swift
@@ -2,7 +2,26 @@ import XCTest
 @testable import Zeitgeist
 
 class TravelTest: XCTestCase {
+    let calendar = Calendar.current
+    var tomorrow: Date!
+    
+    override func setUp() {
+        var oneDay = DateComponents()
+        oneDay.day = 1
+        tomorrow = calendar.date(byAdding: oneDay, to: Date())!
+    }
+    
     func testItTravelsToTomorrow() {
+        Travel.travel(to: tomorrow)
+
+        XCTAssertEqual(tomorrow, Time.current.date)
+        XCTAssertEqual(tomorrow, Travel.now)
+        XCTAssertTrue(Travel.isTraveled)
         
+        Travel.travelBack()
+        
+        XCTAssertNotEqual(tomorrow, Time.current.date)
+        XCTAssertNotEqual(tomorrow, Travel.now)
+        XCTAssertFalse(Travel.isTraveled)
     }
 }
