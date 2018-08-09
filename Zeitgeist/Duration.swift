737dbe9570a4d89e166038b3ded018ef9f727f88
Florian Mielke <f.mielke@systemweit.de>
2018-08-09 12:45:11 +0200

    Add ago to Duration

--- Zeitgeist/Duration.swift
+++ Zeitgeist/Duration.swift
@@ -3,7 +3,34 @@ import Foundation
 public typealias Duration = DateComponents
 
 public extension Duration {
-    public func ago() {
-        
+    public func ago(time: Time = Time.current) -> Time {
+        let date = Calendar.current.date(byAdding: self.inverted(), to: time.date)!
+        return Time(date: date)
+    }
+    
+    func inverted() -> Duration {
+        var inverted = Duration()
+        if let nanosecond = nanosecond {
+            inverted.nanosecond = nanosecond * (-1)
+        }
+        if let second = second {
+            inverted.second = second * (-1)
+        }
+        if let minute = minute {
+            inverted.minute = minute * (-1)
+        }
+        if let hour = hour {
+            inverted.hour = hour * (-1)
+        }
+        if let day = day {
+            inverted.day = day * (-1)
+        }
+        if let month = month {
+            inverted.month = month * (-1)
+        }
+        if let year = year {
+            inverted.year = year * (-1)
+        }
+        return inverted
     }
 }
---
+++ ZeitgeistTests/DurationTest.swift
@@ -0,0 +1,45 @@
+import XCTest
+@testable import Zeitgeist
+
+class DurationTest: XCTestCase {
+    var subject = Duration()
+    
+    override func setUp() {
+        super.setUp()
+        subject = Duration()
+    }
+    
+    override func tearDown() {
+        Travel.travelBack()
+        super.tearDown()
+    }
+    
+    func testCurrentAgo() {
+        let referenceDate = Date.init(timeIntervalSinceReferenceDate: 5)
+        Travel.travel(to: referenceDate)
+        
+        let time = subject.ago()
+        
+        XCTAssertEqual(Time(date: referenceDate), time)
+    }
+
+    func test1DayAgo() {
+        let referenceDate = Date.init(timeIntervalSinceReferenceDate: 5)
+        Travel.travel(to: referenceDate)
+        
+        let time = 1.day.ago()
+
+        let expected = Calendar.current.date(byAdding: .day, value: -1, to: referenceDate)!
+        XCTAssertEqual(Time(date: expected), time)
+    }
+
+    func test2HoursAgo() {
+        let referenceDate = Date.init(timeIntervalSinceReferenceDate: 5)
+        Travel.travel(to: referenceDate)
+        
+        let time = 2.hours.ago()
+        
+        let expected = Calendar.current.date(byAdding: .hour, value: -2, to: referenceDate)!
+        XCTAssertEqual(Time(date: expected), time)
+    }
+}
