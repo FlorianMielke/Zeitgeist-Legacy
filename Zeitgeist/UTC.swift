389a5615662d41db968a2eb13a1caaa8d82f157c
Florian Mielke <f.mielke@systemweit.de>
2019-01-07 17:10:00 +0100

    Consolidate files

--- Zeitgeist/UTC.swift
+++ Zeitgeist/UTC.swift
@@ -1,5 +1,13 @@
 import Foundation
 
+public struct UTCTime: Equatable, Hashable {
+  public let time: Time
+  
+  public init(at date: Date = Travel.now) {
+    time = Time(at: date, calendar: Calendar.utc)
+  }
+}
+
 extension Calendar {
   public static var utc: Calendar {
     var calendar = Calendar.current
--- Zeitgeist/UTCTime.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct UTCTime: Equatable, Hashable {
-  public let time: Time
-  
-  public init(at date: Date = Travel.now) {
-    time = Time(at: date, calendar: Calendar.utc)
-  }
-}
